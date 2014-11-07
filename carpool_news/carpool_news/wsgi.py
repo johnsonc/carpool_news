04f64cbf3c7185c3df5bbaa72701b9b25b78087a
Tomas Raila <tomas.raa@gmail.com>
2014-11-07 18:47:48 +0200

    More fixes for deployment on openshift

--- carpool_news/carpool_news/wsgi.py
+++ carpool_news/carpool_news/wsgi.py
@@ -10,8 +10,22 @@ https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 import os
 import sys
 
-sys.path.append(os.path.join(os.environ['OPENSHIFT_REPO_DIR'], 'carpool_news'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "carpool_news.settings")
+
+# For production
+if 'OPENSHIFT_REPO_DIR' in os.environ:
+    sys.path.append(os.path.join(
+        os.environ['OPENSHIFT_REPO_DIR'], 'carpool_news'))
+
+    virtenv = os.environ['OPENSHIFT_HOMEDIR'] + 'python/virtenv/'
+    os.environ['PYTHON_EGG_CACHE'] = os.path.join(
+        virtenv, 'lib/python2.7/site-packages')
+
+    virtualenv = os.path.join(virtenv, 'bin/activate_this.py')
+    try:
+        execfile(virtualenv, dict(__file__=virtualenv))
+    except IOError:
+        pass
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
